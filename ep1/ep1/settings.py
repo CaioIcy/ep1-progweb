2466194c3f2ce4966f11a8dbdb841d3e40442e83
CaioIcy <caio.nardelli@hotmail.com>
2017-03-22 00:35:49 -0300

    View adjustments / localhost on ALLOWED_HOSTS
    Signed-off-by: CaioIcy <caio.nardelli@hotmail.com>

--- ep1/ep1/settings.py
+++ ep1/ep1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_%74jggog0%$jj4sd_%6l-il9e*@7eio*3m-pp1v=%fpj_x()o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['caioicy.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'caioicy.pythonanywhere.com']
 
 
 # Application definition
